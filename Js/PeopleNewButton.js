2be226a302c9ded8ec3cfe49a13dd20c983f417e
Zhao242 <124598476+Zhao242@users.noreply.github.com>
2024-08-11 21:33:33 +0800

    Update PeopleNewButton.js

--- Js/PeopleNewButton.js
+++ Js/PeopleNewButton.js
@@ -1,15 +1,33 @@
 /**
- * PDNews API 广告去除和底部导航定制 - Surge 脚本
+ * PDNews API 广告去除和底部导航定制
  * 2024-08-11
  */
 const url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 
-// 针对pageInfo的响应去除广告列表
+// 针对pageInfo的响应去除广告和其他不需要的部分
 if (url.includes("https://pdapis.pdnews.cn/api/rmrb-bff-display-zh/display/zh/c/pageInfo/v2")) {
-  if (obj.data && obj.data.compAdList) {
-    obj.data.compAdList = [];
+  if (obj.data) {
+    // 清空 compAdList
+    if (obj.data.compInfo && obj.data.compInfo.compAdList) {
+      obj.data.compInfo.compAdList = [];
+    }
+
+    // 清空 cornersAdv
+    if (obj.data.cornersAdv) {
+      obj.data.cornersAdv = {};
+    }
+
+    // 清空 cornersAdv2
+    if (obj.data.cornersAdv2) {
+      obj.data.cornersAdv2 = [];
+    }
+
+    // 清空 channelInfo
+    if (obj.data.channelInfo) {
+      obj.data.channelInfo = {};
+    }
   }
   body = JSON.stringify(obj);
   $done({body});
